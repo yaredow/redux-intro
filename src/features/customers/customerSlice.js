236e3fdd5bda9061f3d16b2540884309acc705cf
yaredow <yaredyilma11@gmail.com>
2023-08-04 14:32:39 +0300

    fix createdAt

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -13,13 +13,17 @@ const customerSlice = createSlice({
     createCustomer: {
       prepare(fullName, nationalID, createdAt) {
         return {
-          payload: { fullName, nationalID, createdAt },
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
         };
       },
       reducer(state, action) {
         state.fullName = action.payload.fullName;
         state.nationalID = action.payload.nationalID;
-        state.createdAt = new Date().toISOString();
+        state.createdAt = state.payload.createdAt;
       },
     },
   },
@@ -30,34 +34,3 @@ const customerSlice = createSlice({
 
 export const { createCustomer, updateName } = customerSlice.actions;
 export default customerSlice.reducer;
-
-// export default function customerReducer(state = initialStateCustomer, action) {
-//   switch (action.type) {
-//     case "customer/createCustomer":
-//       return {
-//         ...state,
-//         fullName: action.payload.fullName,
-//         nationalID: action.payload.nationalID,
-//         createdAt: action.payload.createdAt,
-//       };
-//     case "customer/updateName":
-//       return { ...state, fullName: action.payload };
-//     default:
-//       return state;
-//   }
-// }
-
-// export function createCustomer(fullName, nationalID, createdAt) {
-//   return {
-//     type: "customer/createCustomer",
-//     payload: {
-//       fullName,
-//       nationalID,
-//       createdAt: new Date().toISOString(),
-//     },
-//   };
-// }
-
-// export function updateName(fullName) {
-//   return { type: "customer/updateName", payload: fullName };
-// }
